ec3bf331aa5fd5315c6915e5a13bb47dab5b8c04
enrict <enric_trillo@outlook.com>
2020-07-08 23:05:47 +0100

    Adding VeeValidate

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="hello">
     <div class="holder">
+      <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill..." v-model="skill" v-validate="'min:5'" name="skill">
+      <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </form>
       <ul>
-        <li v-for="(data, index) in skills" :key='index'>{{index}}. {{ data.skill }}</li>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
       </ul>
+
+      <p>These are the skills that you possess.</p>
     </div>
   </div>
 </template>
@@ -13,10 +19,20 @@ export default {
   name: 'Skills',
   data() {
     return {
+      checked: false,
+      skill: '',
       skills: [
         {"skill": "Vue.js"},
         {"skill": "PHP"}
-      ]
+      ],
+      showAlert: true
+    }
+  },
+  methods: {
+    addSkill(){
+      this.skills.push({skill: this.skill})
+      this.skill = '';
+      console.log('the checkbox value is: '+this.checked);
     }
   }
 }
@@ -24,18 +40,41 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+.holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
 </style>
